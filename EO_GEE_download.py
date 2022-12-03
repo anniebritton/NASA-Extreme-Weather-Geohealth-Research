c75501e58a961f7f4b378404f8dc82c75afa8b02
Annie Britton <99740771+anniebritton@users.noreply.github.com>
2022-12-03 11:40:50 -0600

    Updated script date. misc formatting

--- EO_GEE_download.py
+++ EO_GEE_download.py
@@ -1,11 +1,11 @@
 # Creator: Annie Britton, abritto4@jhu.edu, annie.eliz.britton@gmail.com
-# Date Created: 9/30/2022
+# Date Updated: 12/3/2022
 
 # This script is built to download data from GEE using a batch method.
 # This example uses PRISM data (mean temperature) over the course of 2015.
 # Data sources, assets, and dates are all interchangeable.
 
-# # installs and import libraries, if necessary
+# installs and import libraries, if necessary
 !pip install eefolium
 !pip install pyshp # this is for importing the shapefile
 !pip install geopandas # this is for importing the shapefile
@@ -18,7 +18,7 @@ import pandas as pd
 import geopandas
 import json
 
-# This will initialise ee, and you may need to copy and paste the authentication code
+# this will initialise ee, and you may need to copy and paste the authentication code
 try:
         ee.Initialize()
 except Exception as e:
@@ -30,7 +30,7 @@ except Exception as e:
 # in this case, we have uploaded a shapefile named "Texas" as an asset in GEE
 texas = ee.FeatureCollection("projects/ee-annieelizbritton/assets/Texas")
 
-# Import PRISM data as an image collection
+# import PRISM data as an image collection
 prism = ee.ImageCollection('OREGONSTATE/PRISM/AN81d').filterDate('2015-01-01','2016-01-01').filterBounds(texas).select('tmean')
 
 # batch export to Google Drive
