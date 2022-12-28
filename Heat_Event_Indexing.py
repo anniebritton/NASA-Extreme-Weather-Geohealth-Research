ca4c75a7796eb1a638d64775cb4cbafdf1cb4ff8
Annie Britton <99740771+anniebritton@users.noreply.github.com>
2022-12-28 17:43:48 -0500

    Update and rename Heat_Cold_Event_Indexing.py to Heat_Event_Indexing.py

--- Heat_Event_Indexing.py
+++ Heat_Event_Indexing.py
@@ -1,10 +1,9 @@
 # Creator: Annie Britton, abritto4@jhu.edu, annie.eliz.britton@gmail.com
-# Date Updated: 12/3/2022
 
 # This script is designed to index mean temperature data from a daily census tract-level* CSV
-# and output a CSV with 1s and 0s. Below, heat and cold events are defined by default as two or more days
-# at <5th percentile and >95th percentile of the tract's mean temperature, respectively. 
-# "1" indicates days on which cold/heat waves occur. The thresholds can be adjusted as necessary.
+# and output a CSV with 1s and 0s. Below, heat events are defined by default as two or more days
+# at >95th percentile of the tract's mean temperature. "1" indicates days on which heat waves occur.
+# The threshold can be adjusted as necessary.
 
 # *Column headers in the CSVs indicate tract GEOIDs to differentiate multipart tracts, where a tract may be split and
 # located in two different geographic areas. In this case, there is a unique GEOID for each part of the tract.
@@ -13,8 +12,6 @@
 # import packages
 import pandas as pd
 import numpy as np
-
-######## Heat Event Indexing #########
 
 # read in csv and set index
 heat_df = pd.read_csv("/mnt/local_drive/britton/PRISM_data/PRISM_csvs/tmean_csvs/PRISM_daily_tmean_final.csv")
@@ -44,35 +41,3 @@ heat_df.sum() # check of column summations to see how many days of heat events o
 
 # export heat events
 heat_df.to_csv('/mnt/local_drive/britton/PRISM_data/PRISM_indexing/PRISM_tmean_heat_index.csv')
-
-
-######## Cold Event Indexing #########
-
-# read in csv and set index
-cold_df = pd.read_csv("/mnt/local_drive/britton/PRISM_data/PRISM_csvs/tmean_csvs/PRISM_daily_tmean_final.csv")
-cold_df['Date'] = pd.to_datetime(cold_df['Date'])
-cold_df.set_index('Date', inplace=True)
-
-# function for cold snaps with the default percentile set at 95%
-def coldsnaps(df, perc = 5):
-    
-    # set up the lead and lag columns
-    col_list = df.columns
-    df_lead = df.shift(1)
-    df_lag = df.shift(-1)
-
-    # loop through the columns based on the selected threshold, 
-    # comparing each day to the day before and after
-    for n in range(0, (len(col_list))):
-        threshold = np.percentile(df.iloc[:,n], perc)
-        df.iloc[:,n] = np.where(
-           ((df.iloc[:,n] <= threshold) & ((df_lag.iloc[:,n] <= threshold) | (df_lead.iloc[:,n] <= threshold))), 1, 0
-    )
-        
-# run the coldsnaps function
-coldsnaps(cold_df)
-    
-cold_df.sum() # check of column summations to see how many days of heat events over the study period
-
-# export cold events
-cold_df.to_csv('/mnt/local_drive/britton/PRISM_data/PRISM_indexing/PRISM_tmean_cold_index.csv')
