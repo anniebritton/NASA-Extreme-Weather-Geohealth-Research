f1e3e65a14de216034cdf32e5dc4218f30acc201
Annie Britton <99740771+anniebritton@users.noreply.github.com>
2023-04-05 20:23:22 -0500

    Clarified clean up comment

--- Data-Preprocessing/Heat-Index-Calculation.R
+++ Data-Preprocessing/Heat-Index-Calculation.R
@@ -7,51 +7,50 @@
 # located in two different geographic areas. In this case, there is a unique GEOID for each part of the tract.
 # See here for more info: https://www.census.gov/programs-surveys/geography/guidance/geo-identifiers.html
 
-# load in libraries
 library(tidyverse)
 library(weathermetrics)
 
-# import the census tract extracted variables derived from PRISM and gridMET data
-rmin <- read.csv("/mnt/redwood/local_drive/britton/gridMET_data/gridMET_csvs/V2_exactextract/gridMET_daily_rmin_v2.csv", check.names=FALSE) # min relative humidity
-rmax <- read.csv("/mnt/redwood/local_drive/britton/gridMET_data/gridMET_csvs/V2_exactextract/gridMET_daily_rmax_v2.csv", check.names=FALSE) # max relative humidity
-tmin <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmin_v2.csv", check.names=FALSE) # min temperature
-tmax <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmax_v2.csv", check.names=FALSE) # max temperature
-tmean <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmean_v2.csv", check.names=FALSE) # mean temperature
+# import the premade variables
+rmin <- read.csv("/mnt/redwood/local_drive/britton/gridMET_data/gridMET_csvs/V2_exactextract/gridMET_daily_rmin_v2.csv", row.names="Date", check.names=FALSE)
+rmax <- read.csv("/mnt/redwood/local_drive/britton/gridMET_data/gridMET_csvs/V2_exactextract/gridMET_daily_rmax_v2.csv", row.names="Date", check.names=FALSE)
+tmin <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmin_v2.csv", row.names="Date", check.names=FALSE)
+tmax <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmax_v2.csv", row.names="Date", check.names=FALSE)
+tmean <- read.csv("/mnt/redwood/local_drive/britton/PRISM_data/PRISM_csvs/V2_exactextract/PRISM_tmean_v2.csv", row.names="Date", check.names=FALSE)
 
-# calculate rmean (mean relative humidity) from the mins and maxes
-rmean <- (rmin[-1] + rmax[-1]) / 2
-rmean$Date <- rmin[1]
-rmean <- rmean %>% select(Date, everything())
+# clean up rounding issues in the rmin and rmax data
+rmin_clean <- as.data.frame(lapply(rmin, function(x) ifelse(x > 100, 100, ifelse(x < 0, 0, x))))
+rmax_clean <- as.data.frame(lapply(rmax, function(x) ifelse(x > 100, 100, ifelse(x < 0, 0, x))))
 
-# write a function calculate the raw heat index data
+# calculate rmean from the mins and maxes
+rmean <- (rmin_clean + rmax_clean) / 2
+
+# calculate the raw heat index data
 heat_indexing <- function(t_df, rh_df) {
-  for (i in (length(t_df) - 1)) {
-    t_vec <- t_df[[i+1]]
-    rh_vec <- rh_df[[i+1]]
-    
-    final_hi <- t_df
+  final_hi <- lapply(seq_along(t_df), function(i) {
+    t_vec <- t_df[[i]]
+    rh_vec <- rh_df[[i]]
     
     heat_index <- heat.index(t = t_vec, 
-                            rh = rh_vec,
-                            temperature.metric = "fahrenheit", 
-                            output.metric = "fahrenheit", 
-                            round = 3)
-    final_hi[i+1] <- heat_index
-  }
+                             rh = rh_vec,
+                             temperature.metric = "fahrenheit", 
+                             output.metric = "fahrenheit", 
+                             round = 3)
+    return(heat_index)
+  })
+  
+  final_hi <- as.data.frame(final_hi)
+  names(final_hi) <- names(t_df)
+  rownames(final_hi) <- rownames(t_df)
+  
   return(final_hi)
 }
 
 # run the function across the mean, max, and min data
 mean_hi <- heat_indexing(tmean, rmean)
-max_hi <- heat_indexing(tmax, rmax)
-min_hi <- heat_indexing(tmin, rmin)
-
-# check the dfs
-head(min_hi[1:4])
-head(max_hi[1:4])
-head(mean_hi[1:4])
+max_hi <- heat_indexing(tmax, rmax_clean)
+min_hi <- heat_indexing(tmin, rmin_clean)
 
 # export to csvs to keep raw data
-write.csv(mean_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/mean_heat_index.csv")
-write.csv(max_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/max_heat_index.csv")
-write.csv(min_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/min_heat_index.csv")
+write.csv(mean_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/mean_heat_index_raw_v2.csv")
+write.csv(max_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/max_heat_index_raw_v2.csv")
+write.csv(min_hi, "/mnt/redwood/local_drive/britton/TX_temperature_events/min_heat_index_raw_v2.csv")
